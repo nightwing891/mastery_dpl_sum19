2a8af047cb97975f1c709936cde8bfaeb9ac4074
Tyson Hyland <tyhyland@hotmail.com>
2019-06-26 19:14:24 -0600

    created course and lesson controllers

--- app/controllers/api/courses_controller.rb
+++ app/controllers/api/courses_controller.rb
@@ -1,2 +1,34 @@
 class Api::CoursesController < ApplicationController
+  before_action :authenticate_user, except: [:index]
+  
+  def index
+    render json: Course.all
+  end
+
+  def create
+    @course = Course.new(course_params)
+    if @course.save
+      render json: @course
+    else
+      render json: { errors: @course.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @course = Course.find(params[:id])
+    @course.update(complete: !course.complete)
+    render json: @course
+  end
+
+  def destroy
+    Course.find(params[:id]).destroy
+    render json: { message: 'course deleted' }
+  end
+
+  private
+
+  def course_params
+    params.require(:course).permit(:title, :subtitle, :description, :workbook, :id)
+  end
+
 end
--- app/controllers/api/lessons_controller.rb
+++ app/controllers/api/lessons_controller.rb
@@ -1,2 +1,42 @@
 class Api::LessonsController < ApplicationController
+  before_action: set_course
+
+  def index
+    render json: @course.lessons
+  end
+
+  def create
+    @lesson = @course.lessons.new(lesson_params)
+    if @lesson.save
+      render json: @lesson
+    else
+      render json: { errors: lesson.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @lesson = @course.lessons.find(params[:id])
+    if @lesson.update(lesson_params)
+      render json: @lesson
+    else
+      render json: { errors: @lesson.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @lesson = @course.lessons.find(params[:id])
+    @lesson.destroy
+    render json: { message: 'Lesson deleted' }
+  end
+
+  private
+
+  def lesson_params
+    params.require(:lesson).permit(:instructor, :title, :subtitle, :description, :length, :body, :complete, :id, :course_id)
+  end
+
+  def set_course
+    @course = Course.find(params[:course_id])
+  end
+
 end
