56ec1090bdd0f2b784e4fa08055cc44fd49a3fc8
Candice Marx <candice.marx@hotmail.com>
2019-07-02 20:14:16 -0600

    added formatted reviews and cleaned up homepage

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,23 +1,12 @@
 import React, { Component } from 'react';
 import { Navbar, Footer, Dimmer, Loader } from 'semantic-ui-react';
 import Reviews from './Reviews';
-// import { Navbar, } from 'semantic-ui-react';
-// import { Video, } from ;
-// import { CoursesPrev} from 'some/folder';
-// import { Reviews } from;
 import axios from 'axios';
 import CourseIndex from './Course/CouseIndex';
 
 class Home extends Component {
 
   state = { courses: [], loaded: false }
-{/* <div className="home-videos">
-  <div className="video">
-    <p>Video goes here + link </p>
-    <h4>Description of the video</h4>
-  </div>
-</div> */}
-
 
   setLoaded = () => {
     this.setState({ loaded: true })
@@ -39,8 +28,7 @@ class Home extends Component {
     const { loaded } = this.state
     if(loaded && this.state.courses.length > 0)
     return(
-      <>
-      {/* <Navbar />  */}
+      <>    
       <h1>DevPoint University</h1>
       <p>Learn It, Code It, Build It</p>
       <h2>Let your SKILLS do the talking.</h2>
@@ -51,22 +39,17 @@ class Home extends Component {
           <h4>Description of the video</h4>
         </div>
       </div>
-
       <br />
       <br />
       <br />
+      
       <h2> Create Courses Preview</h2>
-
-      <CourseIndex courses={this.state.courses}/>
+        <CourseIndex courses={this.state.courses}/>
 
       <h2>Reviews</h2>
-      {/* <Reviews /> */}
-
-      {/* <Footer /> */}
-
+        <Reviews />   
       </>
     )
-
     else
       return( 
       <Dimmer active>
@@ -77,23 +60,5 @@ class Home extends Component {
     )
   }
 }
-
-<h2>Reviews</h2>
-
-{/* <Reviews /> */}
-
-<Reviews />
-
-
-
-
- {/* <Footer /> */}
-
-
-/* // 
-//   <Header as="h3" textAlign="center">Devise Auth App</Header>
-// ) */
-
-// make axios call to the database to grab all courses, pass this through CoursePage
 
 export default Home;
