95167ea7136f5a7c19a0ceb4b8002d395c56ccae
Ivanna Brooks <ibrooks@its.nicholls.edu>
2019-07-11 18:55:39 -0600

    hero

--- client/src/components/HomepageHero.js
+++ client/src/components/HomepageHero.js
@@ -1,7 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react'
+import {
+  Button,
+  Container,
+  Header,
+  Icon,
+  Image,
+} from 'semantic-ui-react'
+
+
 
 const HomepageHero = () => (
-  <h1>DevPoint University</h1>
+  <Container text>
+
+    <Header as='h1' style={{
+      fontSize: '10em'
+    }}>
+      DevPoint University
+    </Header>
+
+    <Header as='h5'>
+     Learn It, Code It, Build It.
+    </Header>
+
+   <Header as='h2'>
+      Let your SKILLS do the talking.
+    </Header>
+
+    <Button primary size='huge'>
+       Get Started
+     <Icon name='right arrow' />
+    </Button>
+
+  </Container>
 )
 
 export default HomepageHero;
