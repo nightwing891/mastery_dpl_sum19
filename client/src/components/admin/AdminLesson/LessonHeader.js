832d32540ad33509cedc7ac83f0b4e31098cc889
Candice Marx <candice.marx@hotmail.com>
2019-07-15 20:43:57 -0600

    added adminlesson landing page

--- client/src/components/admin/AdminLesson/LessonHeader.js
+++ client/src/components/admin/AdminLesson/LessonHeader.js
@@ -12,36 +12,13 @@ const LessonHeader = ({
   body,
   complete,
   history
-}) => {
-  const nextLesson = () => {
-    return parseInt(lesson_id) + 1;
-  };
-
-  return (
-    <Container>
-      <Header as="h1">{title}</Header>
-      <Button size="small" onClick={() => history.goBack()}>
-        All Lessons
-      </Button>
-      {/* <Link
-        to={{
-          pathname: `/admin/lesson/${nextLesson()}`,
-          state: {
-            lesson_id,
-            instructor,
-            title,
-            subtitle,
-            description,
-            length,
-            body,
-            complete
-          }
-        }}
-      >
-        <Button size="small">Next Lesson</Button>
-      </Link> */}
-    </Container>
-  );
-};
+}) => (
+  <Container>
+    <Header as="h1">{title}</Header>
+    <Button size="small" onClick={() => history.goBack()}>
+      All Lessons
+    </Button>
+  </Container>
+);
 
 export default withRouter(LessonHeader);
